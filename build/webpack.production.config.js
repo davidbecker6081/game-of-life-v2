4b8c9c032755ffd803ea37054497c40debc77719
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-02-11 12:23:50 +0000

    Add production config and build script

---
+++ build/webpack.production.config.js
@@ -0,0 +1,58 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const src = path.resolve(__dirname, '../src/');
+const dist = path.resolve(__dirname, '../dist/');
+
+module.exports = {
+  entry: `${src}/main.js`,
+
+  output: {
+    filename: '[name]-[hash].js',
+    path: dist,
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: [
+          'babel-loader',
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.json?$/,
+        loader: 'json-loader',
+      },
+      {
+        test: /\.scss$/,
+        loaders: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader!sass-loader',
+        }),
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        drop_console: false,
+      },
+    }),
+    new ExtractTextPlugin('[contenthash].css'),
+    new HtmlWebpackPlugin({
+      template: `${src}/index.tpl.html`,
+    }),
+  ],
+};
