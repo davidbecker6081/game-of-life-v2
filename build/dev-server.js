4b84c689752e4292ef69d0cae84b3c0441c287b8
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-02-11 12:08:44 +0000

    Upgrade to webpack2 and rename webpack directory to build directory

---
+++ build/dev-server.js
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-extraneous-dependencies, no-console */
+
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('./webpack.dev.config');
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true,
+  quiet: false,
+  noInfo: false,
+  stats: {
+    assets: false,
+    colors: true,
+    version: false,
+    hash: false,
+    timings: false,
+    chunks: false,
+    chunkModules: false,
+  },
+}).listen(3000, 'localhost', (err) => {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log('Listening at localhost:3000');
+});
