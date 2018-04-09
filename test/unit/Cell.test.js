570427a1957344ab88d1d622f98e79c277a0f7f7
David Becker <david@beckersite.com>
2018-04-09 15:46:26 -0600

    Add testing for neighbors

--- test/unit/Cell.test.js
+++ test/unit/Cell.test.js
@@ -1,11 +1,11 @@
 const Cell = require('../../src/modules/Cell.js');
 const CellLocation = require('../../src/modules/CellLocation.js');
 const MapOfCells = require('../../src/modules/MapOfCells.js');
-const Canvas = require('canvas');
+// const Canvas = require('canvas');
 
 describe('Cell', () => {
-  let canvas;
-  let ctx;
+  // let canvas;
+  // let ctx;
   let board;
   let xAxisCells;
   let yAxisCells;
@@ -14,16 +14,13 @@ describe('Cell', () => {
   let newCell;
 
   beforeEach(() => {
-    canvas = new Canvas(500, 500);
-    ctx = canvas.getContext('2d');
+    // canvas = new Canvas(500, 500);
+    // ctx = canvas.getContext('2d');
     xAxisCells = 50;
     yAxisCells = 50;
     cellSize = 10;
     locationOfCell = new CellLocation(1, 1);
     newCell = new Cell(locationOfCell, cellSize);
-    board = new MapOfCells();
-    board.initializeMap(xAxisCells, yAxisCells, Cell, CellLocation, cellSize);
-    board.drawMap(xAxisCells, yAxisCells, ctx);
   });
 
   it('should have a location', () => {
@@ -39,16 +36,65 @@ describe('Cell', () => {
     expect(newCell.neighbors).to.equal(0);
   });
   describe('Neighbors', () => {
-    it('should not add to the neighbors count if cell has no neighbors', () => {
-      expect(newCell.neighbors).to.equal(0);
-      newCell.getNeighbors(board.mapOfCells, xAxisCells, yAxisCells);
-      expect(newCell.neighbors).to.equal(0);
+    beforeEach(() => {
+      board = new MapOfCells();
+      board.initializeMap(xAxisCells, yAxisCells, Cell, CellLocation, cellSize);
     });
-    it('should add a neighbor to the initial cell if cell is alive to the left', () => {
-      expect(newCell.neighbors).to.equal(0);
-      board.mapOfCells[1][0].isAlive = true;
-      newCell.getNeighbors(board.mapOfCells, xAxisCells, yAxisCells);
-      expect(newCell.neighbors).to.equal(1);
+
+    describe('Add a neighbor', () => {
+      describe('Cardinal Direcions', () => {
+        it('should add a neighbor if cell is alive to the West', () => {
+          board.mapOfCells[0][1].isAlive = true;
+          newCell.getNeighbors(board.mapOfCells, xAxisCells, yAxisCells);
+          expect(newCell.neighbors).to.equal(1);
+        });
+        it('should add a neighbor if cell is alive to the North', () => {
+          board.mapOfCells[1][0].isAlive = true;
+          newCell.getNeighbors(board.mapOfCells, xAxisCells, yAxisCells);
+          expect(newCell.neighbors).to.equal(1);
+        });
+        it('should add a neighbor if cell is alive to the East', () => {
+          board.mapOfCells[2][1].isAlive = true;
+          newCell.getNeighbors(board.mapOfCells, xAxisCells, yAxisCells);
+          expect(newCell.neighbors).to.equal(1);
+        });
+        it('should add a neighbor if cell is alive to the South', () => {
+          board.mapOfCells[1][2].isAlive = true;
+          newCell.getNeighbors(board.mapOfCells, xAxisCells, yAxisCells);
+          expect(newCell.neighbors).to.equal(1);
+        });
+      });
+      describe('Primary InterCardinal Directions', () => {
+        it('should add a neighbor if cell is alive to the NorthWest', () => {
+          board.mapOfCells[0][0].isAlive = true;
+          newCell.getNeighbors(board.mapOfCells, xAxisCells, yAxisCells);
+          expect(newCell.neighbors).to.equal(1);
+        });
+        it('should add a neighbor if cell is alive to the NorthEast', () => {
+          board.mapOfCells[2][0].isAlive = true;
+          newCell.getNeighbors(board.mapOfCells, xAxisCells, yAxisCells);
+          expect(newCell.neighbors).to.equal(1);
+        });
+        it('should add a neighbor if cell is alive to the SouthEast', () => {
+          board.mapOfCells[2][2].isAlive = true;
+          newCell.getNeighbors(board.mapOfCells, xAxisCells, yAxisCells);
+          expect(newCell.neighbors).to.equal(1);
+        });
+        it('should add a neighbor if cell is alive to the SouthWest', () => {
+          board.mapOfCells[0][2].isAlive = true;
+          newCell.getNeighbors(board.mapOfCells, xAxisCells, yAxisCells);
+          expect(newCell.neighbors).to.equal(1);
+        });
+      });
     });
+    describe('Do not add a neighbor', () => {
+      it('should not add a neighbor if no cells are alive around the initial cell', () => {
+        newCell.getNeighbors(board.mapOfCells, xAxisCells, yAxisCells);
+        expect(newCell.neighbors).to.equal(0);
+      });
+    });
+  });
+  describe('Game Rules', () => {
+
   });
 });
