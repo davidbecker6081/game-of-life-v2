4cce15761dc87bd613e8403e979db537874cb810
David Becker <david@beckersite.com>
2018-04-09 13:48:40 -0600

    Configure tests and Cell class to add a mapOfCells

--- src/modules/Cell.js
+++ src/modules/Cell.js
@@ -1,5 +1,23 @@
-function Cell(location) {
-  this.location = location;
+class Cell {
+  constructor(location) {
+    this.location = location;
+  }
+
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.rect(this.x * this.size, this.y * this.size, this.size, this.size);
+    ctx.strokeStyle = '#c0c3c5';
+    ctx.stroke();
+    ctx.closePath();
+
+    if (this.isAlive) {
+      ctx.fillStyle = '#50C878';
+    } else {
+      ctx.fillStyle = '#f3f3f3';
+    }
+    ctx.fillRect(this.x * this.size, this.y * this.size, this.size, this.size);
+    ctx.stroke();
+  }
 }
 
 module.exports = Cell;
--- test/unit/Cell.test.js
+++ test/unit/Cell.test.js
@@ -1,12 +1,45 @@
 const Cell = require('../../src/modules/Cell.js');
 const CellLocation = require('../../src/modules/CellLocation.js');
-const mocha = require('mocha');
-const chai = require('chai');
-
-global.mocha = mocha;
-global.expect = chai.expect;
+const Canvas = require('canvas');
 
 describe('Cell', () => {
+  // const canvas = {
+  //   height: 500,
+  //   width: 500,
+  // };
+  //
+  // const context = new Object({
+  //   fillStyle: 'red',
+  //   fillRect: () => {},
+  // });
+  //
+  let canvas;
+  let ctx;
+  let mapOfCells;
+  let xAxisCells;
+  let yAxisCells;
+
+  beforeEach(() => {
+    canvas = new Canvas(500, 500);
+    ctx = canvas.getContext('2d');
+    mapOfCells = [];
+    xAxisCells = 50;
+    yAxisCells = 50;
+    for (let i = 0; i < xAxisCells; i++) {
+      const temp = [];
+      for (let j = 0; j < yAxisCells; j++) {
+        const newLocation = new CellLocation(i, j);
+        temp[j] = new Cell(newLocation);
+      }
+      mapOfCells[i] = temp;
+    }
+    for (let i = 0; i < xAxisCells; i++) {
+      for (let j = 0; j < yAxisCells; j++) {
+        mapOfCells[i][j].draw(ctx);
+      }
+    }
+  });
+
   it('should exist', () => {
     const newCell = new Cell(1);
     expect(newCell).to.equal(newCell);
@@ -15,5 +48,6 @@ describe('Cell', () => {
     const locationOfCell = new CellLocation(1, 1);
     const newCell = new Cell(locationOfCell);
     expect(newCell.location).to.equal(locationOfCell);
+    ctx.fillStyle = '#50C878';
   });
 });
